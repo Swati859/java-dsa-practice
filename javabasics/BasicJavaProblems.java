7ecc85c4811c76499ebebeef011987b633edd088
Dell <swatisinghthkur859@gmail.com>
2026-06-12 14:18:34 +0530

    Initial commit

---
+++ javabasics/BasicJavaProblems.java
@@ -0,0 +1,93 @@
+package javabasics;
+
+/*public class BasicJavaProblems {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+    }
+*/
+
+
+/*public class BasicJavaProblems {
+    public static void main(String[]args){
+
+        //calculate area of rectangle
+        int length = 5;
+        int width = 6;
+        int area;
+
+        area = length*width;
+
+        System.out.println(area);
+    }
+}
+*/
+
+//Print Name and Age with Variables
+/*public class BasicJavaProblems {
+    public static void main(String[] args) {
+
+        String name = "Swati";
+        int age = 22;
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
+ */
+
+//Create a Student Report Card
+
+/*public class BasicJavaProblems {
+    public static void main(String[] args) {
+
+
+        String name = "Swati";
+        int studentId = 12345;
+        double score = 95.70;
+        char grade = 'A';
+
+        System.out.println(name);
+        System.out.println(studentId);
+        System.out.println(score);
+        System.out.println(grade);
+
+    }
+}
+ */
+
+// Counting People
+/*public class BasicJavaProblems {
+    public static void main(String []args){
+
+        int peopleInRoom=0;
+
+        peopleInRoom++;
+        peopleInRoom++;
+        peopleInRoom++;
+        peopleInRoom++;
+
+        System.out.println("Enter" + peopleInRoom);
+
+        peopleInRoom--;
+        peopleInRoom--;
+
+        System.out.println("Leave" + peopleInRoom);
+    }
+}
+*/
+
+public class BasicJavaProblems {
+        public static void main(String[]args){
+
+            int peopleInRoom = 4;
+            peopleInRoom++;
+
+            System.out.println(peopleInRoom);
+
+            peopleInRoom--;
+            peopleInRoom--;
+
+            System.out.println(peopleInRoom);
+        }
+    }
